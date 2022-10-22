a8a36860f40c61dd5c7141549768e4538ce5c53e
s-121-11 <s-121-11@gumiforest.ae.krakow.pl>
2022-10-22 08:39:53 +0200

    commit

---
+++ 03-ControlStructures/zadanie12.py
@@ -0,0 +1,4 @@
+suma = 0
+for i in range(100,151):
+    suma = suma+i
+print(suma)
---
+++ 03-ControlStructures/zadanie9.py
@@ -0,0 +1,6 @@
+a = int(input("podaj a: "))
+b = int(input("podaj b: "))
+if a>0 or b>0:
+    print("at least one of them is positive")
+else:
+    print("none of them are positive")
